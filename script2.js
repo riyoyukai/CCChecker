bf242d83180d5d700b727acfdbf8b9efa956d8f2
riyoyukai <riyoyukai@gmail.com>
2017-04-11 18:33:51 -0400

    More

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-var nodes_data = [
-	{
+var data = [
+  {
     "name": "1",
     "type": "change",
     "color": "#99DD33",
@@ -74,6 +74,41 @@ var nodes_data = [
         "radius": 24
       }
     ]
+  },
+  {
+    "name": "3",
+    "type": "change",
+    "color": "#99DD33",
+    "radius": 30,
+    "attributes": []
+  },
+  {
+    "name": "4",
+    "type": "change",
+    "color": "#99DD33",
+    "radius": 30,
+    "attributes": []
+  },
+  {
+    "name": "5",
+    "type": "change",
+    "color": "#99DD33",
+    "radius": 30,
+    "attributes": []
+  },
+  {
+    "name": "6",
+    "type": "change",
+    "color": "#99DD33",
+    "radius": 30,
+    "attributes": []
+  },
+  {
+    "name": "7",
+    "type": "change",
+    "color": "#99DD33",
+    "radius": 30,
+    "attributes": []
   }
 ];
 
@@ -87,121 +122,128 @@ var svg = graph.append('svg')
   .attr('width', width)
   .attr('height', height);
 
-var padding = 3; // separation between same-color nodes
+var padding = 60; // separation between nodes
 // var alpha = .5;
 
 var all_nodes = [];
-var cc_nodes = [];
+var cc_data = []; // The largest node for each cluster
 var attr_nodes = [];
 var links_data = [];
-// var cc_nodes = []; // The largest node for each cluster
+var num = data.length; // number of distinct ccs
 
 var focus;
 var ind = 0;
 
-nodes_data.map(function(entry){
-	var cc = {};
-	cc.ccname = entry.name;
+data.map(function(entry){
+  var cc = {};
+  cc.ccname = entry.name;
   cc.name = entry.name;
   cc.label = entry.name;
   cc.type = entry.type;
-  cc.attributes_data = entry.attributes;
+  cc.attributes_raw = entry.attributes;
   cc.fill = entry.color;
   cc.radius = entry.radius;
   cc.cluster = ind++;
-  cc.x = Math.cos(ind * 2 * Math.PI) * 200 + width / 2 + Math.random(),
-  cc.y = Math.sin(ind * 2 * Math.PI) * 200 + height / 2 + Math.random()
+  cc.x = Math.cos(ind / num * 2 * Math.PI) * 200 + width / 2 + Math.random(),
+  cc.y = Math.sin(ind / num * 2 * Math.PI) * 200 + height / 2 + Math.random()
 
-  cc_nodes.push(cc);
+  cc_data.push(cc);
 });
-	
-var num = cc_nodes.length; // number of distinct ccs
+  
 
-var nodes = [];
 for(var i = 0; i < num; i++){
-  nodes.push(cc_nodes[i]);
-  for(var j = 0; j < cc_nodes[i].attributes_data.length; j++){
+  cc_data[i].attr_data = [];
+  for(var j = 0; j < cc_data[i].attributes_raw.length; j++){
     var attr = {};
-    var attr_data = cc_nodes[i].attributes_data[j];
-    var attr_ind = cc_nodes[i].cluster;
+    var attr_raw = cc_data[i].attributes_raw[j];
+    var attr_ind = cc_data[i].cluster;
 
-    attr.ccname = cc_nodes[i].ccname;
-    attr.name = attr.ccname + attr_data.attribute + attr_data.name;
+    attr.ccname = cc_data[i].ccname;
+    attr.name = attr.ccname + attr_raw.attribute + attr_raw.name;
 
-    attr.fill = attr_data.color;
-    attr.radius = attr_data.radius;
+    attr.fill = attr_raw.color;
+    attr.radius = attr_raw.radius;
     attr.cluster = attr_ind;
-    attr.x = Math.cos(attr_ind * 2 * Math.PI) * 200 + width / 2 + Math.random(),
-    attr.y = Math.sin(attr_ind * 2 * Math.PI) * 200 + height / 2 + Math.random()
+    attr.x = Math.cos(attr_ind / num * 2 * Math.PI) * 200 + width / 2 + Math.random(),
+    attr.y = Math.sin(attr_ind / num * 2 * Math.PI) * 200 + height / 2 + Math.random()
 
-    nodes.push(attr);
+    cc_data[i].attr_data.push(attr);
   }
 }
 
 var simulation = d3.forceSimulation()
-	// keep entire simulation balanced around screen center
-	// .force('center', d3.forceCenter(width/2, height/2))
-	
-	// pull toward center
-	.force('attract', d3.forceAttract()
-		.target([width/2, height/2])
-		.strength(0.01))
+  // keep entire simulation balanced around screen center
+  .force('center', d3.forceCenter(width/2, height/2))
+  
+  // pull toward center
+  .force('attract', d3.forceAttract()
+    .target([width/2, height/2])
+    .strength(0.01))
 
-	// cluster by section
-	.force('cluster', d3.forceCluster()
-		.centers(function (d) { return cc_nodes[d.cluster]; })
-		.strength(0.5)
-		.centerInertia(0.1))
+  // cluster by section
+  .force('cluster', d3.forceCluster()
+    .centers(function (d) { return cc_data[d.cluster]; })
+    .strength(0.5)
+    .centerInertia(0.1))
 
-	// apply collision with padding
-	.force('collide', d3.forceCollide(function (d) { return d.radius + padding; })
-		.strength(0))
+  // apply collision with padding
+  .force('collide', d3.forceCollide(function (d) { return d.radius + padding; })
+    .strength(.1))
 
-	.on('tick', layoutTick)
-	.nodes(nodes);
-	
-// var svg = d3.select('body').append('svg')
-//		 .attr('width', width)
-//		 .attr('height', height);
+  .on('tick', layoutTick)
+  .nodes(cc_data);
 
-var node = svg.selectAll('circle')
-	.data(nodes)
-	.enter().append('circle')
-		.style('fill', function (d) { return d.fill; })
-		.call(d3.drag()
-			.on('start', dragstarted)
-			.on('drag', dragged)
-			.on('end', dragended)
-		);
+var cc_circles = svg.selectAll('circle')
+  .data(cc_data)
+  .enter().append('circle')
+    .style('fill', function (d) { return d.fill; })
+    .call(d3.drag()
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended)
+    );
+
+for(var i = 0; i < cc_data.length; i++){
+  cc_data[i].attr_circles = svg.selectAll('circle')
+  .data(cc_data[i].attr_data)
+  .enter().append('circle')
+    .style('fill', function (d) { return d.fill; })
+    // .call(d3.drag()
+    //   .on('start', dragstarted)
+    //   .on('drag', dragged)
+    //   .on('end', dragended)
+    // )
+;
+}
 
 function dragstarted (d) {
-	if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-	d.fx = d.x;
-	d.fy = d.y;
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
 }
 
 function dragged (d) {
-	d.fx = d3.event.x;
-	d.fy = d3.event.y;
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
 }
 
 function dragended (d) {
-	if (!d3.event.active) simulation.alphaTarget(0);
-	d.fx = null;
-	d.fy = null;
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
 }
 
 // ramp up collision strength to provide smooth transition
 var transitionTime = 3000;
 var t = d3.timer(function (elapsed) {
-	var dt = elapsed / transitionTime;
-	simulation.force('collide').strength(Math.pow(dt, 2) * 0.7);
-	if (dt >= 1.0) t.stop();
+  var dt = elapsed / transitionTime;
+  simulation.force('collide').strength(Math.pow(dt, 2) * 0.7);
+  if (dt >= 1.0) t.stop();
 });
-	
+  
 function layoutTick (e) {
-	node
-		.attr('cx', function (d) { return d.x; })
-		.attr('cy', function (d) { return d.y; })
-		.attr('r', function (d) { return d.radius; });
+  cc_circles
+    .attr('cx', function (d) { return d.x; })
+    .attr('cy', function (d) { return d.y; })
+    .attr('r', function (d) { return d.radius; });
 }
